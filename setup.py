9da59ae73727500a47e40db7124545e7b6138e65
Paul Kienzle <pkienzle@nist.gov>
2011-04-27 10:53:36 -0400

    add setup.py and version info

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+from distutils.core import setup
+import igor
+
+if len(sys.argv) == 1:
+    sys.argv.append('install')
+
+dist = setup(
+        name = 'igor.py',
+        version = igor.__version__,
+        author='Paul Kienzle',
+        author_email='paul.kienzle@nist.gov',
+        url='https://github.com/reflectometry/igor.py',
+        description='Read Igor Pro files from python',
+        long_description=open('README.rst').read(),
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'License :: Public Domain',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            ],
+        py_modules = ['igor'],
+)
+
+# End of file
+
